590502f2c7eaea0d891c0dcab9d2c8ed8964371d
56580 <5658031.rch@gmail.com>
2024-11-26 21:59:13 +0300

    add amplitude

--- start.sh
+++ start.sh
@@ -1,9 +1,17 @@
 #!/bin/sh
 
-alembic downgrade base  # Возвращает базу данных к начальному состоянию (перед миграциями)
+# Вернуться к базовому состоянию
+echo "Returning database to initial state..."
+alembic downgrade base || { echo "Alembic downgrade failed"; exit 1; }
 
-alembic revision --autogenerate
+# Сгенерировать новую миграцию
+echo "Generating new migration..."
+alembic revision --autogenerate -m "Initial migration" || { echo "Alembic revision failed"; exit 1; }
 
-alembic upgrade head
+# Применить миграцию
+echo "Upgrading database to latest version..."
+alembic upgrade head || { echo "Alembic upgrade failed"; exit 1; }
 
-python main.py
+# Запустить приложение
+echo "Starting the application..."
+python main.py || { echo "Application failed to start"; exit 1; }
